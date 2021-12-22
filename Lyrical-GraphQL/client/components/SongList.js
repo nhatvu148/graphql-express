91329ceb52031ab3a7457bab0a51f5c479edf9b1
nhatvu148 <nhatvu148@gmail.com>
2021-12-22 20:11:04 +0900

    mutation

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 const QUERY = gql`
   {
@@ -10,19 +10,47 @@ const QUERY = gql`
   }
 `;
 
+const MUTATION = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
 const SongList = () => {
+  const [newSong, setNewSong] = useState("");
   const { loading, error, data } = useQuery(QUERY);
+  const [addSong, { data: data2, loading: loading2, error: error2 }] =
+    useMutation(MUTATION, {
+      refetchQueries: [QUERY],
+    });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   console.log(data);
   return (
-    <ul className="collection">
-      {data.songs.map(({ id, title }) => (
-        <li key={id} className="collection-item">
-          Title: {title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <p>New title: </p>
+      <input
+        style={{ padding: 5 }}
+        value={newSong}
+        onChange={(input) => setNewSong(input.target.value)}
+      />
+      <button
+        onClick={() => {
+          addSong({ variables: { title: newSong } });
+        }}
+      >
+        Add a song
+      </button>
+      <ul className="collection">
+        {data.songs.map(({ id, title }) => (
+          <li key={id} className="collection-item">
+            Title: {title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
