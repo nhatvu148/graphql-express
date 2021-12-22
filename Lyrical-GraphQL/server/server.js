1d1f2c5b2b26cb958612880496bca13c1931788b
nhatvu148 <nhatvu148@gmail.com>
2021-12-22 18:04:18 +0900

    add docker

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = "mongodb://root:123456789@localhost:27017";
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
